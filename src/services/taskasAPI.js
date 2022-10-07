fecb84a1d06d0eb63f756b4499a6553a0d85c65b
Roman Serediuk <box4rom@gmail.com>
2022-10-07 20:16:27 +0300

    add tasksAPI

---
+++ src/services/taskasAPI.js
@@ -0,0 +1,27 @@
+import { privateAPI } from './http';
+
+const updateTasks = async body => {
+    const { data } = await privateAPI.post('/task/active', body);
+    return data;
+};
+
+const singleTaskActiveToggle = async (id, body) => {
+    await privateAPI.patch(`​/task/single-active/${id}`, body);
+};
+
+const taskCompletedToggle = async (id, body) => {
+    const { data } = await privateAPI.patch(`/task/switch/${id}`, body);
+    return data;
+};
+
+// const addTask = async body => {
+//     const { data } = await privateAPI.post(`/task`, body);
+//     return data;
+// };
+
+export const tasksAPI = {
+    updateTasks,
+    singleTaskActiveToggle,
+    taskCompletedToggle,
+    // addTask,
+};
