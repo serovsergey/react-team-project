b94c9d965b1d57d4848dc1a1077378e446696969
Darina-Shvydiuk <104676845+Darina-Shvydiuk@users.noreply.github.com>
2022-10-07 23:03:46 +0300

    add register button (#10)

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,3 +1,4 @@
+import Button from 'components/common/Button';
 import { useFormik } from 'formik';
 import React from 'react';
 import { useDispatch } from 'react-redux';
@@ -9,60 +10,69 @@ import * as yup from 'yup';
 // import s from './authPage.module.scss';
 
 const validationSchema = yup.object({
-  email: yup
-    .string('Enter email')
-    .min(5, 'Too Short!')
-    .email('Invalid email')
-    .required('Email is required'),
-  password: yup
-    .string('Enter password')
-    .min(8, 'Too Short!')
-    .required('Number is required'),
+    email: yup
+        .string('Enter email')
+        .min(5, 'Too Short!')
+        .email('Invalid email')
+        .required('Email is required'),
+    password: yup
+        .string('Enter password')
+        .min(8, 'Too Short!')
+        .required('Number is required'),
 });
 
 const AuthPage = props => {
-  const dispatch = useDispatch();
-  const formik = useFormik({
-    initialValues: {
-      email: '',
-      password: '',
-    },
-    validationSchema,
-    onSubmit: (values, { resetForm }) => {
-      // onSubmit(values);
-      console.log(values);
-      dispatch(authOperations.login(values))
-        .unwrap()
-        .catch(error => console.log(error));
-      // resetForm();
-    },
-  });
+    const dispatch = useDispatch();
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+        },
+        validationSchema,
+        onSubmit: (values, { resetForm }) => {
+            // onSubmit(values);
+            console.log(values);
+            dispatch(authOperations.login(values))
+                .unwrap()
+                .catch(error => console.log(error));
+            // resetForm();
+        },
+    });
 
-  return (
-    <>
-      <form onSubmit={formik.handleSubmit} className={null}>
-        <label>
-          E-Mail
-          <input
-            name="email"
-            type="text"
-            onChange={formik.handleChange}
-            value={formik.values.email}
-          />
-        </label>
-        <label htmlFor="lastName">
-          Password
-          <input
-            name="password"
-            type="password"
-            onChange={formik.handleChange}
-            value={formik.values.password}
-          />
-        </label>
-        <button type="submit">Login</button>
-      </form>
-    </>
-  );
+    const handleRegister = () => {
+        dispatch(authOperations.register(formik.values))
+            .unwrap()
+            .catch(error => console.log(error));
+    };
+
+    return (
+        <>
+            <form onSubmit={formik.handleSubmit} className={null}>
+                <label>
+                    E-Mail
+                    <input
+                        name="email"
+                        type="text"
+                        onChange={formik.handleChange}
+                        value={formik.values.email}
+                    />
+                </label>
+                <label htmlFor="lastName">
+                    Password
+                    <input
+                        name="password"
+                        type="password"
+                        onChange={formik.handleChange}
+                        value={formik.values.password}
+                    />
+                </label>
+                <Button type="submit">Login</Button>
+                <Button type="button" onClick={handleRegister}>
+                    Rigister
+                </Button>
+            </form>
+        </>
+    );
 };
 
 // AuthPage.propTypes = {};
