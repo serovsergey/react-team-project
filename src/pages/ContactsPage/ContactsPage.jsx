68c1f7a8d7b8a9e948085096a33fd5fcdd61f9f8
Maxxbay <maxxbay@gmail.com>
2022-10-07 23:48:18 +0200

    maxxbay

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import TeamInfoArray from './TeamInfoArray';
 // import PropTypes from 'prop-types';
 
-// import s from './contactsPage.module.scss';
+import s from './contactsPage.module.scss';
 
-const ContactsPage = props => {
-  return <div>ContactsPage</div>;
+const ContactsPage = () => {
+  return (
+    <div className={s.contacts}>
+      <p className={s.contacts__title}>SHCHEKAVYTSIA TEAM</p>
+      <ul className={s.contacts__cardlist}>
+        {TeamInfoArray.map(el => (
+          <li className={s.contacts__card} key={el.id}>
+            <img className={s.contacts__img} src={el.avatar} alt="" />
+            <p className={s.contacts__name}>{el.name}</p>
+            <p className={s.contacts__position}>{el.position}</p>
+            <p className={s.contacts__mail}>
+              <a
+                className={s.contacts__link}
+                href={`mailto:${el.mail}`}
+                target="_blank"
+                rel="noreferrer"
+              >
+                {el.mail}
+              </a>
+            </p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 // ContactsPage.propTypes = {};
