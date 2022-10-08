f14b9a20c96ee0c6d1e3759fc5a466fc247f384e
shuriuss <104681622+shuriuss@users.noreply.github.com>
2022-10-08 20:07:30 +0300

    1

--- src/pages/PlanningPage/PlanningPage.jsx
+++ src/pages/PlanningPage/PlanningPage.jsx
@@ -11,13 +11,15 @@ const PlanningPage = () => {
   const tasks = useSelector(tasksSelectors.getTasks);
   console.log(tasks);
   return (
+    <>
+    <h1 className={s. title}>План на неделю:</h1>
     <div>
       {tasks?.map(({_id, title, reward, imageUrl}) => (
         <div key={_id} className={s.box}>
           <img src={imageUrl} alt={title} style={{ width: 280, height: 194 }} />
          <div className={s.box1}>
           <div>
-          <p className={s.title}>{title}</p>
+          <p className={s.s}>{title}</p>
           <p className={s.reward}>{reward} балла</p>
           </div>
          <TaskScheduleBtn/>
@@ -27,6 +29,7 @@ const PlanningPage = () => {
       
       )}
     </div>
+    </>
   )
 };
 
