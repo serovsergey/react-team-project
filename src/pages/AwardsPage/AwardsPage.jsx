ed466e7e1a83eadfd4d2d5a3eeefc4939fb4fac1
Roman Serediuk <104834083+isITnow@users.noreply.github.com>
2022-10-08 12:46:56 +0300

    add TaskCompletedInd component (#16)

--- src/pages/AwardsPage/AwardsPage.jsx
+++ src/pages/AwardsPage/AwardsPage.jsx
@@ -1,10 +1,16 @@
+import TaskCompletedInd from 'components/common/TaskCompletedInd';
 import React from 'react';
 // import PropTypes from 'prop-types';
 
 // import s from './awardsPage.module.scss';
 
 const AwardsPage = props => {
-  return <div>AwardsPage</div>;
+    return (
+        <div>
+            AwardsPage
+            <TaskCompletedInd />
+        </div>
+    );
 };
 
 // AwardsPage.propTypes = {};
