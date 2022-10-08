2a5156a0608eb0cd72c178df458c5951fe965b87
shuriuss <104681622+shuriuss@users.noreply.github.com>
2022-10-08 21:13:31 +0300

    Shuriuss/planning (#28)
    * 1
    
    * 1
    
    * update
    
    * Delete operations.auth.js
    
    Co-authored-by: Serov Sergey <32988243+serovsergey@users.noreply.github.com>

--- src/pages/PlanningPage/PlanningPage.jsx
+++ src/pages/PlanningPage/PlanningPage.jsx
@@ -1,10 +1,31 @@
+import Card from 'components/common/Card';
+import TaskScheduleBtn from 'components/common/TaskScheduleBtn';
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import tasksSelectors from 'redux/tasks/selector.tasks';
+
 // import PropTypes from 'prop-types';
 
-// import s from './planningPage.module.scss';
+import s from './planningPage.module.scss';
 
 const PlanningPage = () => {
-  return <div>PlanningPage</div>;
+    const tasks = useSelector(tasksSelectors.getTasks);
+    console.log(tasks);
+
+    return (
+        <>
+            <h1 className={s.title}>План на неделю:</h1>
+            <ul>
+                {tasks?.map(({_id, title, reward, imageUrl }) => (
+                    <li key={_id}>
+                        <Card title={title} reward={reward} imageUrl={imageUrl}>
+                            <TaskScheduleBtn />
+                        </Card>
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
 };
 
 // PlanningPage.propTypes = {};
