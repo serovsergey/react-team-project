75260b21023b8b3ee81c0d196169b3b54ecd81bd
Roman Serediuk <box4rom@gmail.com>
2022-10-09 12:31:11 +0300

    add styles/change structure

--- src/pages/AwardsPage/AwardsPage.jsx
+++ src/pages/AwardsPage/AwardsPage.jsx
@@ -1,15 +1,15 @@
+import React from 'react';
+import { useMediaQuery } from 'react-responsive';
 import Button from 'components/common/Button';
 import Card from 'components/common/Card';
-// import TaskCompletedInd from 'components/common/TaskCompletedInd';
-import TaskScheduleBtn from 'components/common/TaskScheduleBtn';
-import React from 'react';
+import Container from 'components/Container';
+import ToggleSwitch from 'components/toggleSwitch/ToggleSwitch';
+import { ReactComponent as PrizesIcon } from '../../assets/svg/Prizes.svg';
 import ProgressBar from 'components/ProgressBar';
 import ProgressBarMobile from 'components/ProgressBarMobile';
-import { useMediaQuery } from 'react-responsive';
+import s from './awardsPage.module.scss';
 
 // import PropTypes from 'prop-types';
-
-// import s from './awardsPage.module.scss';
 
 const Desktop = ({ children }) => {
     const isDesktop = useMediaQuery({ minWidth: 1280 });
@@ -26,23 +26,28 @@ const Mobile = ({ children }) => {
 
 const AwardsPage = props => {
     return (
-        <div>
-            AwardsPage
-            <Button>TEST</Button>
-            <Card>
-                {/* <TaskCompletedInd /> */}
-                <TaskScheduleBtn />
-            </Card>
+        <Container>
+            <div className={s.prize__label}>
+                <PrizesIcon />
+                <h2 className={s.title}> my prizes</h2>
+            </div>
+
             <Desktop>
                 <ProgressBar />
             </Desktop>
             <Tablet>
                 <ProgressBar />
             </Tablet>
+
+            <Card>
+                <ToggleSwitch />
+            </Card>
+            <Button>confirm</Button>
             <Mobile>
                 <ProgressBarMobile />
             </Mobile>
-        </div>
+        </Container>
+        // </div>
     );
 };
 
