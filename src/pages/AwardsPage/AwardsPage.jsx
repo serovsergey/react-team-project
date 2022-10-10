989f6ba553ffa64dc7af5513a45a8024008a45b2
Roman Serediuk <box4rom@gmail.com>
2022-10-10 13:58:16 +0300

    bug fix, styles fix
    AwardsPage - add local state, add styles,
    ProgressBarMobile - add fixed position
    ToggleSwitch - add new icon

--- src/pages/AwardsPage/AwardsPage.jsx
+++ src/pages/AwardsPage/AwardsPage.jsx
@@ -1,5 +1,7 @@
+import s from './awardsPage.module.scss';
 import React from 'react';
 import { useMediaQuery } from 'react-responsive';
+import { useDispatch, useSelector } from 'react-redux';
 import Button from 'components/common/Button';
 import Card from 'components/common/Card';
 import Container from 'components/Container';
@@ -7,13 +9,9 @@ import ToggleSwitch from 'components/toggleSwitch/ToggleSwitch';
 import { ReactComponent as PrizesIcon } from '../../assets/svg/Prizes.svg';
 import ProgressBar from 'components/ProgressBar';
 import ProgressBarMobile from 'components/ProgressBarMobile';
-import s from './awardsPage.module.scss';
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
-import giftsOperations from 'redux/gifts/operations.gifts';
 import giftsSelectors from 'redux/gifts/selector.gifts';
-import TaskCompletedInd from 'components/common/TaskCompletedInd';
-import TaskScheduleBtn from 'components/common/TaskScheduleBtn';
+import giftsOperations from 'redux/gifts/operations.gifts';
+import { useState } from 'react';
 
 // import PropTypes from 'prop-types';
 
@@ -30,47 +28,60 @@ const Mobile = ({ children }) => {
     return isMobile ? children : null;
 };
 
-const AwardsPage = props => {
+const AwardsPage = () => {
     const gifts = useSelector(giftsSelectors.getGifts);
+    const isLoading = useSelector(giftsSelectors.getIsLoading);
+    const [giftIds, setGiftIds] = useState([]);
+    const dispatch = useDispatch();
     console.log(gifts);
-    // const dispatch = useDispatch();
-    // useEffect(() => {
-    //     dispatch(giftsOperations.getGifts());
-    // }, [dispatch]);
-    return (
-        <Container>
-            <div className={s.prize__label}>
-                <PrizesIcon />
-                <h2 className={s.title}> my prizes</h2>
-            </div>
 
-            <Desktop>
-                <ProgressBar />
-            </Desktop>
-            <Tablet>
-                <ProgressBar />
-            </Tablet>
-            <ul className={s.list}>
-                {gifts?.map(({ id, title, price, imageUrl, isSelected }) => (
-                    <li key={id} className={s.item}>
-                        <Card
-                            id={id}
-                            title={title}
-                            reward={price}
-                            imageUrl={imageUrl}
-                        >
-                            <ToggleSwitch />
-                        </Card>
-                    </li>
-                ))}
-            </ul>
-            <div className={s.btn}>
-                <Button>confirm</Button>
-            </div>
+    const handleConfirm = () => {
+        dispatch(giftsOperations.buyGifts());
+    };
+
+    return (
+        <>
+            <Container>
+                <div className={s.prize__label}>
+                    <PrizesIcon />
+                    <h2 className={s.title}> my prizes</h2>
+                </div>
+
+                <Desktop>
+                    <ProgressBar />
+                </Desktop>
+                <Tablet>
+                    <ProgressBar />
+                </Tablet>
+                <ul className={s.list}>
+                    {gifts?.map(
+                        ({ id, title, price, imageUrl, isSelected }) => (
+                            <li key={id} className={s.item}>
+                                <Card
+                                    id={id}
+                                    title={title}
+                                    reward={price}
+                                    imageUrl={imageUrl}
+                                >
+                                    <ToggleSwitch
+                                        isChecked={isSelected}
+                                        awardId={id}
+                                    />
+                                </Card>
+                            </li>
+                        )
+                    )}
+                </ul>
+                <div className={s.btn}>
+                    <Button type="submit" onClick={handleConfirm}>
+                        confirm
+                    </Button>
+                </div>
+            </Container>
             <Mobile>
                 <ProgressBarMobile />
             </Mobile>
-        </Container>
+        </>
     );
 };
 
