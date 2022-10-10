1e9cfc0314d56cd91f7f4f9a8ee39af30cde1631
Roman Serediuk <104834083+isITnow@users.noreply.github.com>
2022-10-10 16:24:15 +0300

    Roma/feature awards page (#42)
    * add styles/change structure
    
    * Update AwardsPage.jsx
    
    * add awards-list/add styles
    
    * bug fix, styles fix
    
    AwardsPage - add local state, add styles,
    ProgressBarMobile - add fixed position
    ToggleSwitch - add new icon

--- src/pages/AwardsPage/AwardsPage.jsx
+++ src/pages/AwardsPage/AwardsPage.jsx
@@ -1,5 +1,7 @@
+import s from './awardsPage.module.scss';
 import React from 'react';
 import { useMediaQuery } from 'react-responsive';
+import { useDispatch, useSelector } from 'react-redux';
 import Button from 'components/common/Button';
 import Card from 'components/common/Card';
 import Container from 'components/Container';
@@ -7,7 +9,9 @@ import ToggleSwitch from 'components/toggleSwitch/ToggleSwitch';
 import { ReactComponent as PrizesIcon } from '../../assets/svg/Prizes.svg';
 import ProgressBar from 'components/ProgressBar';
 import ProgressBarMobile from 'components/ProgressBarMobile';
-import s from './awardsPage.module.scss';
+import giftsSelectors from 'redux/gifts/selector.gifts';
+import giftsOperations from 'redux/gifts/operations.gifts';
+import { useState } from 'react';
 
 // import PropTypes from 'prop-types';
 
@@ -24,30 +28,60 @@ const Mobile = ({ children }) => {
     return isMobile ? children : null;
 };
 
-const AwardsPage = props => {
+const AwardsPage = () => {
+    const gifts = useSelector(giftsSelectors.getGifts);
+    const isLoading = useSelector(giftsSelectors.getIsLoading);
+    const [giftIds, setGiftIds] = useState([]);
+    const dispatch = useDispatch();
+    console.log(gifts);
+
+    const handleConfirm = () => {
+        dispatch(giftsOperations.buyGifts());
+    };
+
     return (
-        <Container>
-            <div className={s.prize__label}>
-                <PrizesIcon />
-                <h2 className={s.title}> my prizes</h2>
-            </div>
+        <>
+            <Container>
+                <div className={s.prize__label}>
+                    <PrizesIcon />
+                    <h2 className={s.title}> my prizes</h2>
+                </div>
 
-            <Desktop>
-                <ProgressBar />
-            </Desktop>
-            <Tablet>
-                <ProgressBar />
-            </Tablet>
-
-            <Card>
-                <ToggleSwitch />
-            </Card>
-            <Button>confirm</Button>
+                <Desktop>
+                    <ProgressBar />
+                </Desktop>
+                <Tablet>
+                    <ProgressBar />
+                </Tablet>
+                <ul className={s.list}>
+                    {gifts?.map(
+                        ({ id, title, price, imageUrl, isSelected }) => (
+                            <li key={id} className={s.item}>
+                                <Card
+                                    id={id}
+                                    title={title}
+                                    reward={price}
+                                    imageUrl={imageUrl}
+                                >
+                                    <ToggleSwitch
+                                        isChecked={isSelected}
+                                        awardId={id}
+                                    />
+                                </Card>
+                            </li>
+                        )
+                    )}
+                </ul>
+                <div className={s.btn}>
+                    <Button type="submit" onClick={handleConfirm}>
+                        confirm
+                    </Button>
+                </div>
+            </Container>
             <Mobile>
                 <ProgressBarMobile />
             </Mobile>
-        </Container>
-    );
+        </>
 };
 
 // AwardsPage.propTypes = {};
