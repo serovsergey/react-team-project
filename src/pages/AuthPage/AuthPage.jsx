8bb615af0211eb158b7e728eadc2132393010ce6
Darina-Shvydiuk <darinakabakova@gmail.com>
2022-10-11 14:29:30 +0300

    add universal hook useMediaQuery

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,22 +1,9 @@
 import AuthForm from 'components/AuthForm';
 
 import Footer from 'components/Footer/Footer';
-import { useMediaQuery } from 'react-responsive';
+import { MediaQuery } from '../../hooks/useMediaQuery';
 
 import s from './authPage.module.scss';
-
-const Desktop = ({ children }) => {
-    const isDesktop = useMediaQuery({ minWidth: 1280 });
-    return isDesktop ? children : null;
-};
-const Tablet = ({ children }) => {
-    const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1279 });
-    return isTablet ? children : null;
-};
-const Mobile = ({ children }) => {
-    const isMobile = useMediaQuery({ minWidth: 320, maxWidth: 767 });
-    return isMobile ? children : null;
-};
 
 const AuthPage = () => {
     return (
@@ -26,31 +13,31 @@ const AuthPage = () => {
             </h1>
 
             <AuthForm></AuthForm>
-            <Desktop>
+            <MediaQuery.Desktop>
                 <Footer></Footer>
-            </Desktop>
-            <Tablet>
+            </MediaQuery.Desktop>
+            <MediaQuery.Tablet>
                 <Footer></Footer>
-            </Tablet>
+            </MediaQuery.Tablet>
 
-            <Desktop>
+            <MediaQuery.Desktop>
                 <div className={s.inner_images_bp1280px}>
                     <div className={s.inner_images_bp1280px__family}></div>
                     <div className={s.inner_images_bp1280px__bulb}></div>
                     <div className={s.inner_images_bp1280px__family2}></div>
                     <div className={s.inner_images_bp1280px__robot}></div>
                 </div>
-            </Desktop>
-            <Tablet>
+            </MediaQuery.Desktop>
+            <MediaQuery.Tablet>
                 <div className={s.inner_images_bp768px}>
                     <div className={s.inner_images_bp768px__family}></div>
                     <div className={s.inner_images_bp768px__bulb}></div>
                     <div className={s.inner_images_bp768px__family2}></div>
                 </div>
-            </Tablet>
-            <Mobile>
+            </MediaQuery.Tablet>
+            <MediaQuery.Mobile>
                 <div className={s.inner_images_bp320px}></div>
-            </Mobile>
+            </MediaQuery.Mobile>
         </section>
     );
 };
