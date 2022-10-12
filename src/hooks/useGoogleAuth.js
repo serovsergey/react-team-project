849caa341713fc89302dce0a9d86517f2d1429f7
Darina-Shvydiuk <darinakabakova@gmail.com>
2022-10-12 13:57:23 +0300

    Update useGoogleAuth.js

--- src/hooks/useGoogleAuth.js
+++ src/hooks/useGoogleAuth.js
@@ -1,19 +1,24 @@
 import { useEffect, useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+// import { useSearchParams } from 'react-router-dom';
+import authSelectors from '../redux/auth/selector.auth';
+import userOperations from '../redux/user/operations.user';
 
 export const useGoogleAuth = () => {
-    const [searchParams] = useSearchParams();
-    const token = searchParams.get('token');
+    const dispatch = useDispatch();
+    // const [searchParams] = useSearchParams();
+    // const token = searchParams.get('token');
+    const token = useSelector(authSelectors.getToken);
 
     const [isLoading, setIsLoading] = useState(false);
-
-    //  const [userInfo, setUserInfo] = useState(null);
 
     useEffect(() => {
         if (token) {
             setIsLoading(true);
+            dispatch(userOperations.getUserInfo());
+
             // TODO Logic
         }
-    }, [token]);
+    }, [token, dispatch]);
     return isLoading;
 };
