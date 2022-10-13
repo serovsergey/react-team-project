c13df9ab6e908efcb42948ee99ad2ef2038ba3bb
shuriuss <104681622+shuriuss@users.noreply.github.com>
2022-10-13 23:58:55 +0300

    Shuriuss/checkdays (#98)
    * update
    
    * add cheked day
    
    * add update OK

--- src/components/common/TaskScheduleBtn/TaskScheduleBtn.jsx
+++ src/components/common/TaskScheduleBtn/TaskScheduleBtn.jsx
@@ -3,22 +3,48 @@ import { BsPlusLg } from 'react-icons/bs';
 // import PropTypes from 'prop-types';
 import s from './taskScheduleBtn.module.scss';
 import CheckDay from '../CheckDay';
+import { inetialStateCheckDays } from '../CheckBox/CheckBox';
+import { useDispatch } from 'react-redux';
+import tasksOperations from 'redux/tasks/operations.tasks';
 
 const TaskScheduleBtn = ({ buttonId }) => {
     const btnRef = useRef();
+    const ref = useRef(null);
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const dispatch = useDispatch();
 
     useEffect(() => {
-        const name = btnRef.current.name;
-        console.log(name);
-    });
+        const handleClick = event => {
+            // console.log(ref);
+            // console.log(event.target);
+            // console.log(ref.current?.contains(event.target));
+            if (!ref.current?.contains(event.target)) {
+                setIsModalOpen(false);
+            }
+        };
+
+        if (isModalOpen) {
+            setTimeout(() => {
+                window.addEventListener('click', handleClick);
+            }, 0);
+        }
+        return () => window.removeEventListener('click', handleClick);
+    }, [isModalOpen]);
 
     const handleclick = () => {
         setIsModalOpen(prev => !prev);
+
+        if (isModalOpen) {
+            const id = btnRef.current.name;
+            console.log(id);
+            const taskData = inetialStateCheckDays;
+            console.log(taskData);
+            dispatch(tasksOperations.setActiveSingle({id, taskData}));
+        }
     };
 
-    return (
-        <>
+        return (
+        <div ref={ref}>
             {isModalOpen ? (
                 <button
                     ref={btnRef}
@@ -41,7 +67,7 @@ const TaskScheduleBtn = ({ buttonId }) => {
                 </button>
             )}
             {isModalOpen && <CheckDay />}
-        </>
+        </div>
     );
 };
 
