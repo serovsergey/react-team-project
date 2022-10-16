f4e1443132246a196cdc6c9be727d30beb20a2bb
Darina-Shvydiuk <darinakabakova@gmail.com>
2022-10-16 12:40:18 +0300

    fix toast add

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -40,7 +40,7 @@ const AuthForm = () => {
                 .unwrap()
                 .catch(error =>
                     toast.error(
-                        t(`Login is failed with message`) + ' ' + error.message
+                        t(`Login is failed with message: `) + error.message
                     )
                 );
         },
@@ -52,7 +52,7 @@ const AuthForm = () => {
             .unwrap()
             .catch(error =>
                 toast.error(
-                    t(`Register is failed with message:`) + ' ' + error.message
+                    t(`Register is failed with message: `) + error.message
                 )
             );
     };
