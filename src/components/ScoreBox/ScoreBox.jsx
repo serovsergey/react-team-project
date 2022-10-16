a928ece85db8238e34e548d9329a99e55310db04
Pavel <104570430+VPagent@users.noreply.github.com>
2022-10-16 16:42:44 +0300

    addPortal (#137)

--- src/components/ScoreBox/ScoreBox.jsx
+++ src/components/ScoreBox/ScoreBox.jsx
@@ -2,12 +2,17 @@ import { useSelector } from 'react-redux';
 import userSelectors from 'redux/user/selector.user';
 import s from '../ScoreBox/scoreBox.module.scss';
 import { useTranslation } from 'react-i18next';
+import ChangeLanguage from 'components/ChangeLanguage';
+import { useMediaQuery } from 'react-responsive';
 
 const ScoreBox = () => {
     const { t } = useTranslation();
     //  console.log(i18n)
     const userScore = useSelector(userSelectors.getUserBalance);
+    const isTabletScreen = useMediaQuery({ query: '(min-width: 768px)' });
+
     return (
+        <>
         <div className={s.scoreBox}>
             <p className={s.item}>
                 {t(`Score`)}
@@ -15,7 +20,9 @@ const ScoreBox = () => {
                 {t(`balance:`)}
             </p>
             <p className={s.item}>{userScore}</p>
+        {isTabletScreen && <div className={s.languageWrapper}><ChangeLanguage /></div>}
         </div>
+        </>
     );
 };
 
